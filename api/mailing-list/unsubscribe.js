0816ce00c04de19d2d803a73b788aaa533d74bd1
Tom Feltwell <tfeltwell@gmail.com>
2020-08-20 12:32:30 +0100

    Implements unsubscribe functionality that uses the UUID provided in the confirmation email. Basic styling on the confirmation page

--- api/mailing-list/unsubscribe.js
+++ api/mailing-list/unsubscribe.js
@@ -29,28 +29,33 @@ const Email = sequelize.define('Email', {
 module.exports = async(req, res) => {
   try {
     if (req.query && req.query.authid) {
-      let result = await Email.findAll({
+      // UUID are unique
+      let result = await Email.findOne({
         where: {
           subid: req.query.authid
         }
       });
-      if (result && result.length > 0) {
-        // TO DO - implement logic to handle the query and remove said record
-        console.log(`remove ${req.query.authid} from db`);
-        sendResponse(req, res, 200, "OK");
+      if (result) {
+        console.log(`Email found matching ${result.subid}: ${result.email}`);
+        await result.destroy();
+        sendResponse(req, res, 200, "Thank you, this email address has been successfully unsubscribed.");
+      } else {
+        // UUID is not unique (shouldn't happen), or no result returned
+        sendResponse(req, res, 400, "This email address is already unsubscribed.");
       }
     } else {
       // Failed on formatting, no authid
-      sendResponse(req, res, 400, "Failed");
+      console.log("No query sent or no authid");
+      sendResponse(req, res, 400, "Unfortunately we were unable to unsubscribe this address. Please follow the link in your confirmation email.");
     }
   } catch (error) {
-    sendResponse(req, res, 400, "Failed");
+    sendResponse(req, res, 400, "Unfortunately we were unable to unsubscribe this address. Please follow the link in your confirmation email.");
   }
 }
 
 function sendResponse (req, res, status, message) {
-  res.status(status);
-  res.json({
-    body: message
-  });
+  // res.status(status);
+  res.writeHead(status, { 'Content-Type': 'text/html' });
+  res.write(`<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Unsubscribe from the Metro Futures Mailing List</title></head><body><h1>Metro Futures Mailing List</h1><p>${message}</p><p>If you are having trouble, please <a href="mailto:support@metrofutures.org.uk">contact us</a>.</p><p>Visit the main site: <a href="https://metrofutures.org.uk">metrofutures.org.uk</a></p></body></html>`);
+  res.end();
 }
