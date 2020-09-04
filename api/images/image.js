53533d39fef43dab95f9469b29f3820b20423451
Tom Feltwell <tfeltwell@gmail.com>
2020-09-04 13:49:51 +0100

    Begins implementing image API

---
+++ api/images/image.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+
+module.exports = async(req, res) => {
+  if(!req.cookies.mfsid){
+    console.log('Unauthorized');
+    sendResponse(req, res, 403, "Unauthorized");
+  } else {
+    try {
+      if(true) {
+        sendResponse(req, res, 200, returnObjects);
+      } else {
+        sendResponse(req, res, 400, "Unable to connect");
+      }
+    } catch (error) {
+      sendResponse(req, res, 400, "Error");
+    }
+  }
+}
+
+function sendResponse(req, res, status, message) {
+  res.status(status);
+  res.send(message);
+}
