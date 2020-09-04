8f948b8af7dd9c11be5bbf637c6bcb17a7b5a7ef
Tom Feltwell <tfeltwell@gmail.com>
2020-09-04 14:59:34 +0100

    Variable for decoding the get request and building the URL

--- api/images/image.js
+++ api/images/image.js
@@ -1,11 +1,22 @@
 require('dotenv').config()
 
+let options = {
+  cam: 0,
+  opt0: 0,
+  opt1: 0,
+  opt2: 0,
+  opt3: 0,
+  opt4: 0,
+  opt5: 0
+}
+
 module.exports = async(req, res) => {
   if(!req.cookies.mfsid){
     console.log('Unauthorized');
     sendResponse(req, res, 403, "Unauthorized");
   } else {
     try {
+      // Get options from the get request 
       if(true) {
         sendResponse(req, res, 200, returnObjects);
       } else {
@@ -20,4 +31,9 @@ module.exports = async(req, res) => {
 function sendResponse(req, res, status, message) {
   res.status(status);
   res.send(message);
+}
+
+function urlFormatter(options) {
+  // Pass in object of all options
+  console.log(options);
 }
