31658abec47f2958c42bb44e2d85d2aeaa430792
Tom Feltwell <tfeltwell@gmail.com>
2020-09-13 21:50:01 +0100

    Demographic API endpoint - rough implementation * As detailed in Notion, this endpoint will take any of 0 - 6 pieces of demographic information and add it to the DB * The cookie is used to track the users so it is pulled from the cookie and stored alongside this data * Can't iterate the req.body so checks if any of the data items are present and adds them * Only tested with Postman so far but appears to work ok. * Added an .env variable for the database name, so that we are working in a -dev environment here and can automatically switch to production

--- api/response/participant.js
+++ api/response/participant.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const pg = require('pg');  // Needed by Vercel
-const { Sequelize } = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = new Sequelize(process.env.pg_db, process.env.pg_user, process.env.pg_pass, {
   host: process.env.pg_host,
   dialect: 'postgres',
@@ -13,16 +13,80 @@ const sequelize = new Sequelize(process.env.pg_db, process.env.pg_user, process.
   port: 25060,
 });
 
+
+// Sequelize db model here
+const Response = sequelize.define('Response', {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: Sequelize.INTEGER
+  },
+  questionid: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  response: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  sessid: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+}, {
+  tableName: process.env.API_PPT_TABLE_NAME
+}
+);
+
+// - 0: Local Authority
+// - 1: age
+// - 2: gender
+// - 3: ethnicity
+// - 4: disability
+// - 5: journey purpose
+// - 6: frequency
+
 module.exports = async(req, res) => {
   if(!req.cookies.mfsid){
     console.log('Unauthorized');
     sendResponse(req, res, 403, "Unauthorized");
   } else {
-    console.log('valid cookie');
+    console.log('valid cookie', req.cookies.mfsid);
+    let sessid = req.cookies.mfsid
     try {
-      await sequelize.authenticate();
-      console.log('connected successfully');
-      sendResponse(req, res, 200, "Received")
+      // await sequelize.authenticate();
+      await sequelize.sync({ alter: true });  // This makes sure the DB matches the model, and will change it
+      if (req.body) {
+        // Implement these two so we can trigger an error response
+        let submitted = 0;
+        let success = 0;
+        // Can't iterate the req.body because we don't have a bodyParser and its not iterable
+        if (req.body["0"]) { 
+          await addResponse(0, req.body[0], sessid) 
+        }
+        if (req.body["1"]) { 
+          await addResponse(1, req.body[1], sessid) 
+        }
+        if (req.body["2"]) { 
+          await addResponse(2, req.body[2], sessid) 
+        }
+        if (req.body["3"]) { 
+          await addResponse(3, req.body[3], sessid) 
+        }
+        if (req.body["4"]) { 
+          await addResponse(4, req.body[4], sessid) 
+        }
+        if (req.body["5"]) { 
+          await addResponse(5, req.body[5], sessid) 
+        }
+        if (req.body["6"]) { 
+          await addResponse(6, req.body[6], sessid) 
+        }
+        sendResponse(req, res, 200, "Success")
+      } else {
+        sendResponse(req, res, 400, "Incorrect formatting")
+      }
     } catch (error) {
       console.log('unable to connect', error)
       sendResponse(req, res, 400, "Unable to connect")
@@ -33,4 +97,12 @@ module.exports = async(req, res) => {
 function sendResponse(req, res, status, message) {
   res.status(status);
   res.send(message);
+}
+
+async function addResponse(qid, data, sessid) {
+  console.log(`submitting ${qid}, data: ${data}, sessid: ${sessid}`)
+  await Response.create({questionid: qid, response: data, sessid: sessid}).then(function() {
+    console.log(`Added ${qid}: ${data} to DB`)
+  }) 
+  // Add an error catch here
 }
