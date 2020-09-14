384df02b084d9414d504cda5d9b9859471aa7188
Tom Feltwell <tfeltwell@gmail.com>
2020-09-14 17:02:00 +0100

    Changes wording as requested in snag

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -2,8 +2,8 @@
   <div>
     <b-dropdown id="dropdown-1" text="Metro Futures" class="m-md-2 menu" variant="primary">
       <b-dropdown-item><router-link to="/about">About Metro Futures</router-link></b-dropdown-item>
-      <b-dropdown-item><router-link to="/explore">Explore Your Metro</router-link></b-dropdown-item>
-      <b-dropdown-item><router-link to="/configure">Configure Your Metro</router-link></b-dropdown-item>
+      <b-dropdown-item><router-link to="/explore">Your Metro</router-link></b-dropdown-item>
+      <b-dropdown-item><router-link to="/configure">Your Choices</router-link></b-dropdown-item>
       <!-- <b-dropdown-item><router-link to="/journeys">Your Journeys</router-link></b-dropdown-item> -->
       <b-dropdown-item>Terms and Conditions</b-dropdown-item>
       <b-dropdown-item>Privacy Policy</b-dropdown-item>
