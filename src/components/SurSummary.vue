2efe7316424078148ef698e688eebd25bb5861fe
katecourt <kate.court@newcastle.ac.uk>
2020-09-14 15:48:55 +0100

    share facebook and twitter setup

--- src/components/SurSummary.vue
+++ src/components/SurSummary.vue
@@ -132,8 +132,35 @@
                     <p id="option1" class="option" @click="toggleFeatures()"><b-icon-info-circle></b-icon-info-circle> VIEW STANDARD FEATURES </p>
                     <p id="option2" class="option "><b-icon-printer></b-icon-printer> PRINT </p>
                     <p id="option3" class="option "><b-icon-envelope></b-icon-envelope> SEND PDF</p>
-                    <p id="option4" class="option text-center"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="1em" height="1em" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path d="M22.46 6c-.77.35-1.6.58-2.46.69c.88-.53 1.56-1.37 1.88-2.38c-.83.5-1.75.85-2.72 1.05C18.37 4.5 17.26 4 16 4c-2.35 0-4.27 1.92-4.27 4.29c0 .34.04.67.11.98C8.28 9.09 5.11 7.38 3 4.79c-.37.63-.58 1.37-.58 2.15c0 1.49.75 2.81 1.91 3.56c-.71 0-1.37-.2-1.95-.5v.03c0 2.08 1.48 3.82 3.44 4.21a4.22 4.22 0 0 1-1.93.07a4.28 4.28 0 0 0 4 2.98a8.521 8.521 0 0 1-5.33 1.84c-.34 0-.68-.02-1.02-.06C3.44 20.29 5.7 21 8.12 21C16 21 20.33 14.46 20.33 8.79c0-.19 0-.37-.01-.56c.84-.6 1.56-1.36 2.14-2.23z" fill="#626262"/></svg>
-                        SHARE</p>
+                        <b-dropdown  variant="primary" id="dropdown-1" text="SHARE" class="m-md-2">
+                            <b-dropdown-item>
+                                <ShareNetwork
+                                    network="facebook"
+                                    url="https://news.vuejs.org/issues/180"
+                                    title="Say hi to Vite! A brand new, extremely fast development setup for Vue."
+                                    description="This week, I’d like to introduce you to 'Vite', which means 'Fast'. It’s a brand new development setup created by Evan You."
+                                    quote="The hot reload is so fast it\'s near instant. - Evan You"
+                                    hashtags="vuejs,vite"
+                            >
+                               Facebook
+                            </ShareNetwork>
+                            </b-dropdown-item>
+                            <b-dropdown-item>
+                                <ShareNetwork
+                                        network="twitter"
+                                        url="https://news.vuejs.org/issues/180"
+                                        title="Say hi to Vite! A brand new, extremely fast development setup for Vue."
+                                        twitter-user="@thebestmetro"
+                                        hashtags="vuejs,vite"
+                                >
+                                    Twitter
+                                </ShareNetwork>
+                            </b-dropdown-item>
+                            <b-dropdown-item>Third Action</b-dropdown-item>
+                        </b-dropdown>
+
+<!--                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="1em" height="1em" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path d="M22.46 6c-.77.35-1.6.58-2.46.69c.88-.53 1.56-1.37 1.88-2.38c-.83.5-1.75.85-2.72 1.05C18.37 4.5 17.26 4 16 4c-2.35 0-4.27 1.92-4.27 4.29c0 .34.04.67.11.98C8.28 9.09 5.11 7.38 3 4.79c-.37.63-.58 1.37-.58 2.15c0 1.49.75 2.81 1.91 3.56c-.71 0-1.37-.2-1.95-.5v.03c0 2.08 1.48 3.82 3.44 4.21a4.22 4.22 0 0 1-1.93.07a4.28 4.28 0 0 0 4 2.98a8.521 8.521 0 0 1-5.33 1.84c-.34 0-.68-.02-1.02-.06C3.44 20.29 5.7 21 8.12 21C16 21 20.33 14.46 20.33 8.79c0-.19 0-.37-.01-.56c.84-.6 1.56-1.36 2.14-2.23z" fill="#626262"/></svg>-->
+<!--                        SHARE-->
 
                 </b-col>
             </b-row>
