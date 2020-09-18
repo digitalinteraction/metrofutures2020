635100ff774d677ebe21da7e56705ed5a3e31163
Tom Feltwell <tfeltwell@gmail.com>
2020-09-18 14:33:38 +0100

    Added some persona information from the official document

--- src/data/personas.js
+++ src/data/personas.js
@@ -2,37 +2,37 @@ export default [
   {
     id: 0,
     name: "Mary",
-    desc: "Mary, 67, is heading to the coast to meet...",
+    desc: "Mary is recently retired and lives in Mount Pleasant, Gateshead. Her friends have been retired for a few years longer and met weekly whilst Mary was working. Now that Mary has more free time, she's able to meet them during the day for a coffee, do a little shopping and have a catch up.",
     img: "mary.jpg"
   },
   {
     id: 1,
     name: "Sanjeev",
-    desc: "Sanjeev came to Newcastle Leciester to study...",
+    desc: "Sanjeev is a radiologist at the Sunderland Royal Hospital. Sanjeev and his husband, Drew recently moved to Ponteland to be closer to the countryside. When Sanjeev started working in Sunderland he would drive there, but now prefers to cycle to Callerton Parkway and take his bike on the Metro when he can. He uses the Metro twice a day, five days a week, at different times according to the shifts he is working.",
     img: "sanjeev.jpg"
   },
   {
     id: 2,
     name: "Polly",
-    desc: "Polly is 15 years old, and lives with her parents...",
+    desc: "Polly is 15 years old, and lives with her parents in Whitley Bay. She gets the Metro to school every day, starting her journey early in the morning at 6:45 am. Polly prefers her own company and likes to get to school (and back) as quickly as possible. Her main interests are books and music.",
     img: "polly.jpg"
   },
   {
     id: 3,
     name: "Robert",
-    desc: "Robert is heading to work. He is a powered wheel chair user...",
+    desc: "Robert lives in South Gosforth and is a systems analyst for a large company. He gets the Metro to and from work every day between South Gosforth and Four Lane Ends. In his spare time Robert enjoys photography and real ale. Robert uses a wheelchair.",
     img: "robert.jpg"
   },
   {
     id: 4,
     name: "Desmond",
-    desc: "Desmond lives alone in West Park, South Shields...",
+    desc: "Desmond lives alone in West Park, South Shields but his two grown up children live nearby. He enjoys his own company, although he has a hectic social life. He’s really got back into music recently and often uses the Metro to go to the Sage Gateshead and the 02 Academy in Newcastle. Desmond has lost some of his sight due to complications with his diabetes.",
     img: "desmond.jpg"
   },
   {
     id: 5,
     name: "Jessica",
-    desc: "Jessica is Gateshead born and bred...",
+    desc: "Jessica and Gary live in Bensham, Gateshead with their two children 3-year-old Holly and 6-month old Jake. Jessica is a teaching assistant but is currently on maternity leave with Jake and has Holly at home with her. Jessica and the children use the Metro a couple of times a week for day trips to the coast or to Newcastle or Sunderland for museums and shopping.",
     img: "jessica.jpg"
   },
 ]
