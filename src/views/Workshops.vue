faebe2b2c648797f3f4509ffe283068411f23da0
Tom Feltwell <tfeltwell@gmail.com>
2020-09-18 15:47:17 +0100

    Adds actual workshop content

--- src/views/Workshops.vue
+++ src/views/Workshops.vue
@@ -12,21 +12,19 @@
       </b-col>
     </b-row>
 
-    <b-row>
+    <b-row v-for="(shop, index) in workshops" :key="index">
       <b-col>
         <b-card
-          title="Shared Spaces"
+          :title="shop.title"
           tag="article"
-          
-          
-          header="Tuesday 22nd September, 18:00-19:00"
+          :header="shop.dateTime"
           header-tag="header"
         >
           <b-card-text>
-            People told us that new trains needed spaces that could be used by all, including those with bags or luggage, wheelchair users, people with pushchairs, and cyclists. This workshop will evaluate the new trains with an emphasis on how well they work as shared spaces. Places are limited.
+            {{ shop.desc }}
           </b-card-text>
 
-          <b-button href="https://newcastleuniversity.zoom.us/meeting/register/tJAqdOCgrDMpHNXM7uPrfWK1yBWQECeGQNQz" variant="primary">Register Here</b-button>
+          <b-button :href="shop.link" variant="primary">Register Here</b-button>
         </b-card>
       </b-col>
     </b-row>
@@ -88,6 +86,20 @@ export default {
     MainHeader,
     Footer,
   },
+  data() {
+    return {
+      workshops: [
+        {title: "Shared Spaces", desc: "People told us that new trains needed spaces that could be used by all, including those with bags or luggage, wheelchair users, people with pushchairs, and cyclists. This workshop will evaluate the new trains with an emphasis on how well they work as shared spaces. Places are limited.", link: "https://newcastleuniversity.zoom.us/meeting/register/tJAqdOCgrDMpHNXM7uPrfWK1yBWQECeGQNQz", dateTime: "Tuesday 22nd September, 18:00-19:00"},
+        {title: "Busy Trains", desc: "People told us that trains needed to be usable at busy times when many passengers are on board. This workshop will evaluate the new trains with an emphasis on their usability at busy times. Places are limited.", link: "https://newcastleuniversity.zoom.us/meeting/register/tJ0pfu6srDkiGdZ1o6_nM007I4SqGuMgcxZs", dateTime: "Wednesday 23rd September, 11:00-12:00"},
+        {title: "Personal Wellbeing", desc: "People told us that the new trains needed features to ensure passengers’ personal wellbeing such as physical comfort and feeling safe when travelling alone or late at night. This workshop will evaluate the new trains with an emphasis on personal wellbeing. Places are limited.", link: "https://newcastleuniversity.zoom.us/meeting/register/tJEkcumtqDooGt1tKW8xQkZbxsytvQJCZNEJ", dateTime: "Thursday 24th September, 18:00-19:00"},
+        {title: "Safety and Accessibility", desc: "People told us that the new trains need to have features that make them safe and accessible for everyone – those with impairments and non-disabled people. This workshop will evaluate the new trains with an emphasis on safety and accessibility. Places are limited.", link: "https://newcastleuniversity.zoom.us/meeting/register/tJMuf-iurToqGdKFEftsodXnnYYdIo9ip5Hi", dateTime: "Friday 25th September, 11:00-12:00"},
+        {title: "Busy Trains", desc: "Another opportunity to attend a workshop on this topic. Places are limited.", link: "https://newcastleuniversity.zoom.us/meeting/register/tJMpfumgqzwrHNH0l1cEI_IG4uunVLA1Yx6Q", dateTime: "Tuesday 29th September, 18:00-19:00"},
+        {title: "Shared Spaces", desc: "Another opportunity to attend a workshop on this topic. Places are limited.", link: "https://newcastleuniversity.zoom.us/meeting/register/tJwlfuGrpjwpHtDVTZ7t9_GIzoPPl4qP2Z6o", dateTime: "Wednesday 30th September, 11:00-12:00"},
+        {title: "Safety and Accessibility", desc: "Another opportunity to attend a workshop on this topic. Places are limited.", link: "https://newcastleuniversity.zoom.us/meeting/register/tJMvdeusrTwsGNXdkTpgcNQwrDy8IpWLF3On", dateTime: "Thursday 1st October, 18:00-19:00"},
+        {title: "Personal Wellbeing", desc: "Another opportunity to attend a workshop on this topic. Places are limited.", link: "https://newcastleuniversity.zoom.us/meeting/register/tJAuceuupjIqHtwCnlqHIaKDL7gnV0byn5H9", dateTime: "Friday 2nd October, 11:00-12:00"},
+      ]
+    }
+  }
 
 }
 </script>
