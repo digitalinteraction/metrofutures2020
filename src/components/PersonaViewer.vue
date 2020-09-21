d7426f84d3173bffa03cf39bfee0adc8a7a295eb
Tom Feltwell <tfeltwell@gmail.com>
2020-09-21 12:45:38 +0100

    Puts data for personas into json file for speed. Begins implementation of interface and functionality

--- src/components/PersonaViewer.vue
+++ src/components/PersonaViewer.vue
@@ -5,14 +5,67 @@
       <b-container>
         <b-row>
           <b-col class="border">
-            <!-- {{ persona }} -->
+            Breadcrumbs here
           </b-col>
         </b-row>
         <b-row>
-          <b-col class="border">
-            Main Image here
+          <b-col class="border col-lg-9 col-12">
+
+            <video class="embed-video" ref="mainVideo" controls="true">
+              <!-- poster="https://cdn.metrofutures.org.uk/doors/PosterExterior.png" -->
+              <source src="https://cdn.metrofutures.org.uk/personas/p0/Polly1.mp4" type="video/mp4" >
+              <!-- <p>Your browser doesn't support HTML5 video. Here is a <a :href="data.visual">link to the video</a> instead.</p> -->
+            </video>
+            
+            <div class="controls-video text-center"> 
+              <b-iconstack font-scale="2" v-on:click="videoPlayButton()" v-show="!videoFinished">
+                <b-icon stacked icon="circle-fill" variant="primary"></b-icon>
+                <b-icon stacked icon="play-fill"  v-show="!videoPlaying"></b-icon>
+                <b-icon stacked icon="stop-fill"  v-show="videoPlaying"></b-icon>
+              </b-iconstack>
+
+              <b-iconstack font-scale="2" v-on:click="replayButton()" v-show="videoFinished">
+                <b-icon stacked icon="circle-fill" variant="primary"></b-icon>
+                <b-icon stacked icon="arrow-repeat"></b-icon>
+              </b-iconstack>
+            </div>
+
           </b-col>
-          <b-col class="border">Questions will appear over here</b-col>
+          <b-col class="border col-lg-3 col-12">
+            <span class="question-wrapper" v-show="videoFinished">
+              <div class="question-text">Question text here, blah blah blah?</div>
+              <!-- {{ personaInfo.desc }} -->
+              
+              <!-- Options -->
+              
+              <!-- Likert -->
+              <div class="likert">
+                <b-form-rating
+                class="likert_item"
+                v-model="likertRating"
+                icon-empty="circle"
+                icon-full="circle-fill"
+                variant="warning"
+                >
+                </b-form-rating>
+              </div>
+              
+              <!-- Free comment -->
+              <div class="surveyFreeText" >
+                <label class="calvert" for="survey-text-response">Leave Feedback (optional) </label>
+                <textarea
+                v-model="commentText"
+                placeholder="Got something to say? Let us know..."
+                class="form-control"
+                rows="2"
+                >
+                </textarea>
+              </div>
+              
+              <!-- Submit button -->
+              <b-button block variant="outline-secondary">Submit</b-button>
+            </span>
+          </b-col>
         </b-row>
         <b-row>
           <Footer></Footer>
@@ -36,13 +89,41 @@ export default {
     return {
       personaName: "",
       personaInfo: {},
+      currentStageId: 0,
+      videoEl: false,
+      videoPlaying: false,
+      videoFinished: false,
+      // Form data
+      likertRating: 0,
+      commentText: "",
     }
   },
   computed: {
     ...mapGetters(["getPersonas"]) 
   },
   methods: {
-    
+    nextScene() {
+      // Get next stage
+      // From that get video URL
+      // and get next question
+    },
+    videoPlay() {
+
+    },
+    videoStop() {
+      console.log("video has stopped")
+      this.videoFinished = true;
+    },
+    videoPlayButton() {
+      // If we are finished don't do anything
+      if(!this.videoFinished) {
+        // Play if we have paused, pause if we are playing
+        console.log("play/pause current video")
+      }
+    },
+    videoNext() {
+      // Get the next video and load it into the element
+    }
   },
 
   beforeRouteUpdate (to, from, next) {
@@ -54,11 +135,15 @@ export default {
     next(false);  // Will cancel the route (use if not valid route)
   },
   mounted() {
-    // Get persona name from route
-    this.personaName = this.$route.params.persona
-    this.personaInfo = this.$store.getters.getPersonaByName(this.personaName)
     // Need a guard to make sure we are accessing ones that exist
+    this.personaName = this.$route.params.persona  // Get persona name from route
+    this.personaInfo = this.$store.getters.getPersonaByName(this.personaName)
 
+    // Get the video element
+    this.videoEl = this.$refs.mainVideo
+    this.videoEl.addEventListener('ended', this.videoStop);
+
+    // Get stages and questions
   }
 
 }
@@ -69,6 +154,10 @@ export default {
 
   .border {
     border: 1px solid black;
+  }
+
+  .embed-video {
+    width: 500px;
   }
 
 </style>
---
+++ src/data/persona_data.js
@@ -0,0 +1,31 @@
+export default [
+  {
+    id: 5, 
+    name: "Mary",
+    stages: [
+      {id: 0, }
+    ],
+    questions: [
+      {id: 0, text: "", options: "bike", comment: true},
+    ]
+  },
+  {
+    id: 3, 
+    name: "Polly",
+    stages: [
+      {id: 0, videoUrl: "Polly1.mp4", nextId: 1, questionId: 0},
+      {id: 1, videoUrl: "Polly2.mp4", nextId: 2, questionId: 1},
+      {id: 2, videoUrl: "Polly3.mp4", nextId: 3, questionId: 2},
+      {id: 3, videoUrl: "Polly4.mp4", nextId: 4, questionId: 3},
+      {id: 4, videoUrl: "Polly5.mp4", nextId: 5, questionId: 4},
+      {id: 5, videoUrl: "Polly6.mp4", nextId: null, questionId: null},
+    ],
+    questions: [
+      {id: 0, text: "How well is the multipurpose space indicated?", options: "", comment: true, likert: true},
+      {id: 1, text: "Which bike stand is best for where Polly likes to stand? ", options: "bike", comment: true, likert: false},
+      {id: 2, text: "How clear is the passenger information screen?", options: "", comment: true, likert: true},
+      {id: 3, text: "Which combination of pole and door colours, and floor markings are best for Polly?", options: "colours", comment: true, likert: false},
+      {id: 4, text: "How clear are the door light and sound indications?", options: "", comment: true, likert: true},
+    ]
+  }
+]
