a16293de925fbcf96941c9318875e4df7ed4d490
Tom Feltwell <tfeltwell@gmail.com>
2020-11-10 16:41:48 +0000

    Adds bar chart type for when real data is in

--- src/views/Results.vue
+++ src/views/Results.vue
@@ -20,6 +20,11 @@
         </p>
       </b-col>
     </b-row>
+    <b-row>
+      <b-col>
+        <LineChart :chart-data="datacollection"></LineChart>
+      </b-col>
+    </b-row>
 
     <b-row>
       <b-col>
@@ -31,6 +36,11 @@
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque dolorum, porro accusamus officiis laudantium, labore natus voluptatem ab temporibus facilis nobis dolore maiores, iusto aut deserunt sunt. Enim, autem quasi!
         </p>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col>
+        <BarChart :chart-data="datacollection"></BarChart>
       </b-col>
     </b-row>
 
@@ -67,15 +77,46 @@
 <script>
 import Footer from "@/components/Footer";
 import MainHeader from '@/components/MainHeader.vue'
+import LineChart from '@/charts/LineChart.js'
+import BarChart from '@/charts/BarChart.js'
 
 export default {
     name: "Results",
     components: {
         MainHeader,
         Footer,
+        LineChart,
+        BarChart,
     },
-
-
+    data () {
+      return {
+        datacollection: null
+      }
+    },
+    mounted () {
+      this.fillData()
+    },
+    methods: {
+      fillData () {
+        this.datacollection = {
+          labels: [this.getRandomInt(), this.getRandomInt()],
+          datasets: [
+            {
+              label: 'Data One',
+              backgroundColor: '#f87979',
+              data: [this.getRandomInt(), this.getRandomInt()]
+            }, {
+              label: 'Data One',
+              backgroundColor: '#f87979',
+              data: [this.getRandomInt(), this.getRandomInt()]
+            }
+          ]
+        }
+      },
+      getRandomInt () {
+        return Math.floor(Math.random() * (50 - 5 + 1)) + 5
+      }
+    }
 }
 </script>
 
